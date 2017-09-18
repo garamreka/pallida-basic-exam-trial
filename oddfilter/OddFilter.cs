d6b76b0e63bc39efd43c50536b7516c70a81deec
Reka Garamvolgyi <garam.reka@gmail.com>
2017-09-18 14:51:43 +0200

    OddFilter (feat): OddFilter function added

--- oddfilter/OddFilter.cs
+++ oddfilter/OddFilter.cs
@@ -10,7 +10,7 @@ namespace OddFilter
             // Create an OddFilter function that takes a list as a parameter,
             // and returns a new list with every odd element from the orignal list
             List<int> oddElements = OddFilter(new List<int> { 1, 2, 3, 4, 5 });
-
+            
             // In case of the example input above, the given PrintList function should print 1 3 5 
             PrintList(oddElements);
 
@@ -22,6 +22,16 @@ namespace OddFilter
             foreach (int element in list)
             {
                 Console.Write("{0} ", element);
+            }
+        }
+        public static void OddFilter (List<int> list)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i]%2 == 0)
+                {
+                    Console.Write(list[i]);
+                }
             }
         }
     }
